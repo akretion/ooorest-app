5eca4e43816be19fa658647e738e38c2b1f1f2d1
Raphael Valyi <rvalyi@akretion.com>
2015-09-07 04:26:04 -0300

    mailgateway dev setting exemple with Gmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,19 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"
+  ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :user_name            => "smtp-user",
+  :password             => 'smtp-password',
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+  }
+
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
