0cb6679b84f57b2f944906817aa0e3455aa1007c
Raphael Valyi <rvalyi@akretion.com>
2015-10-23 12:37:40 -0200

    API access only if devise_api_secret set

--- app/controllers/devise_api_controller.rb
+++ app/controllers/devise_api_controller.rb
@@ -1,35 +1,44 @@
 class DeviseApiController < ActionController::Base
 
   def create
-    ensure_auth
-    user = User.create email: params[:email]
-    user.send_reset_password_instructions
-    respond_to do |format|
-      format.json { render json: user.id, layout: false }
+    with_auth do
+      user = User.create email: params[:email]
+      user.send_reset_password_instructions
+      respond_to do |format|
+        format.json { render json: user.id, layout: false }
+      end
     end
   end
 
   def update
-    ensure_auth
-    user = User.find(params[:id])
-    user.email = params[:email]
-    user.save
-    respond_to do |format|
-      format.json { render json: true, layout: false }
+    with_auth do
+      user = User.find(params[:id])
+      user.email = params[:email]
+      user.save
+      respond_to do |format|
+        format.json { render json: true, layout: false }
+      end
     end
   end
 
   def destroy
-    ensure_auth
-    User.destroy(params[:id])
-    respond_to do |format|
-      format.json { render json: true, layout: false }
+    with_auth do
+      User.destroy(params[:id])
+      respond_to do |format|
+        format.json { render json: true, layout: false }
+      end
     end
   end
 
   private
 
-  def ensure_auth
-    # TODO
+  def with_auth
+    if ENV['DEVISE_API_SECRET'] == params[:devise_api_secret]
+      yield
+    else
+      respond_to do |format|
+        format.json { render json: {message: 'unauthorized access', code: 403}, layout: false }
+      end
+    end
   end
 end
